bd1cf12c32b5a36f85842ff7f2118baff9307d46
Hira Khan <phr.hirakhan@gmail.com>
2024-11-19 21:36:20 +0100

    add code for multi page dashboard layout design

---
+++ app_pages/multipage.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+
+# Class to generate multiple Streamlit pages using an object oriented approach
+class MultiPage:
+
+    def __init__(self, app_name) -> None:
+        self.pages = []
+        self.app_name = app_name
+
+        st.set_page_config(
+            page_title=self.app_name,
+            page_icon="🖥️",
+            layout="wide"  # Set layout to wide to remove default sidebar
+        )
+
+    def add_page(self, title, func) -> None:
+        self.pages.append({"title": title, "function": func})
+
+    def run(self):
+        page = st.sidebar.radio('Menu', self.pages, format_func=lambda page: page['title'])
+        page['function']()
